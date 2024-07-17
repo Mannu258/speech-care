0a4335c78e2f5366a747ef261d7e4b645ff08d61
Your Name <you@example.com>
2024-07-17 16:49:27 +0530

    added some features

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Details(models.Model):
+    Name = models.CharField(max_length=60)
+    Email = models.EmailField()
+    Mobile = models.CharField(max_length=10)
+    Message = models.TextField()
+    Date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{self.Name} {self.Date}"
+
+class Email(models.Model):
+    Email = models.EmailField()
+    Date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{self.Email} {self.Date}"
--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Details,Email
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        Name = request.POST.get('name')
+        email = request.POST.get('email')
+        Mobile = request.POST.get('mobile')
+        Message = request.POST.get('msg')
+        if Name and Email and Mobile and Message:
+            Details.objects.get_or_create(Name=Name,Email=email,Mobile=Mobile,Message=Message)
+            return render(request,'Thank-You.html')
+        if email:
+            Email.objects.get_or_create(Email=email)
+            return render(request,"index.html")
     return render(request,"index.html")
